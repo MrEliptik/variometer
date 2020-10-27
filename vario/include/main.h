8ffa1dbe9b140d20976c174076c44956999e12b3
Victor <victormeunier.dev@gmail.com>
2020-10-27 12:32:54 +0100

    move to ESP32 (buzzer missing)

--- vario/include/main.h
+++ vario/include/main.h
@@ -1,18 +1,82 @@
 #include <Arduino.h>
+#include "SPI.h"
+
 #include <barometer.h>
+#include <display.h>
 
 // Need to use GPIO number
-#define GPS_PPS_PIN 0
-#define LED_PIN 3
-#define BUZZER_PIN 14
-#define SLCK_PIN 5
-#define MISO_PIN 6
-#define MOSI_PIN 7
-#define CS_PIN 8
+//TODO : update these pins with the ESP32
+#define GPS_PPS_PIN             0
+#define LED_PIN                 3
+#define BUZZER_PIN              14
+#define SLCK_PIN                5
+#define MISO_PIN                6
+#define MOSI_PIN                7
+#define CS_PIN                  8
 
-#define JOYSTICK_PIN A0
+#define JOYSTICK_PIN            36
 
-int readButtons(int pin);
+// ######### GENERAL #########
+#define LOOP_HERTZ              60                                  // Hz
+#define LOOP_TIME               1.0/(float)LOOP_HERTZ
+#define LOOP_TIME_MS            LOOP_TIME*1000
+#define LOOP_TIME_US            LOOP_TIME*1000000
+#define DISPLAY_REFRESH_RATE    15                                  // Hz
+#define DISPLAY_TIME            1.0/(float)DISPLAY_REFRESH_RATE
+#define DISPLAY_TIME_MS         DISPLAY_TIME*1000
+#define DISPLAY_TIME_US         DISPLAY_TIME*1000000
+#define BAROMETER_REFRESH_RATE  2                                   // Hz
+#define BAROMETER_TIME          1.0/(float)BAROMETER_REFRESH_RATE
+#define BAROMETER_TIME_MS       BAROMETER_TIME*1000
+#define BAROMETER_TIME_US       BAROMETER_TIME*1000000
+
+enum JOYSTICK {
+    UP,
+    LEFT,
+    DOWN,
+    PRESS,
+    RIGHT,
+    NONE
+};
+
+enum LCD {
+    ORIENTATION_PORTRAIT,
+    ORIENTATION_LANDSCAPE
+};
+
+enum VARIO_STATE {
+    HOME,
+    HOME_VARIO,
+    HOME_GPS,
+    HOME_COMPASS,
+    HOME_ALL,
+    HOME_SETTINGS,
+    VARIO,
+    GPS,
+    COMPASS,
+    ALL,
+    SETTINGS
+};
+
+/*
+Hardware hookup
+
+LCD Pin                 Arduino Pin
+-------                 -----------
+ 1  Vcc                 Vcc
+ 2  Gnd                 Gnd
+ 3  Gnd                 Gnd or N/C
+ 4                      N/C
+ 5                      N/C
+ 6  LED (backlight)     D3         -> not available on my LCD ?
+ 7  CLK                 GPIO18 (SCK)
+ 8  SDI                 GPIO23 (MOSI)
+ 9  RS                  GPIO19
+10  RST                 GPIO4
+11  CS                  GPIO5
+*/
+
+JOYSTICK readButtons(int pin);
 void beep(float smoothed_ascent_rate);
 // Screen res 
 //176*220
--- vario/src/main.cpp
+++ vario/src/main.cpp
@@ -1,7 +1,16 @@
 #include <main.h>
 
 // Last measure time
-double lastTime;
+double last_time;
+
+// Loop timing
+unsigned long loop_time;
+unsigned long display_time;
+unsigned long barometer_time;
+
+bool first_time_state = false;
+
+bool time_to_display = false;
 
 // Variometer buzzer
 float climb_threshold = 0.5;
@@ -15,32 +24,34 @@ float time_started_tone = 0.0;
 float curr_freq = 556.0;
 float curr_waiting_time = 1000.0;
 
-double last_smoother_ascent_rate = 1.0;
+double last_smoother_ascent_rate = 0.0;
+double avg_ascent_rate = 0.0;
 const float SMOOTHING_WEIGHT = 0.15;
 
+JOYSTICK joystick_previous_state = NONE;
 
-enum {
-    UP,
-    LEFT,
-    DOWN,
-    PRESS,
-    RIGHT,
-    NONE
-};
-
-int joystick_previous_state = NONE;
+VARIO_STATE vario_state = VARIO;
+// Different previous state to have a first update
+VARIO_STATE previous_vario_state = HOME_SETTINGS;
 
 void setup() {
     Serial.begin(115200);
+    delay(500);
     Serial.print("VARIOMETER v0.1 booting..");
+    setupDisplay(ORIENTATION_LANDSCAPE, Terminal12x16);
+    displayStr(20, 50, "VARIOMETER v0.1");
+    displayStr(50, 80, "booting..");
 
-    pinMode(JOYSTICK_PIN, INPUT_PULLUP);
+    pinMode(JOYSTICK_PIN, INPUT);
     
     setupBarometer();
-    lastTime = millis();
+    delay(500);
+    last_time = millis();
+    display_time = micros();
 }
 
 void loop() {
+    loop_time = micros();
     /*
     Serial.println("");
     Serial.println("###################");
@@ -57,35 +68,182 @@ void loop() {
     Serial.print("Altitude delta: ");
     Serial.println(getAltitudeDelta());
     */
-    int b = readButtons(JOYSTICK_PIN);
-    //Serial.println(b);
-    if (b != NONE) Serial.println(b);
-    /*
-    float ascent_rate = getAscentRate(lastTime);
-    Serial.print("Ascent rate: ");
-    Serial.print(ascent_rate);
-    Serial.println(" m/s");
-    float smoothed_ascent_rate = SMOOTHING_WEIGHT * ascent_rate + (1-SMOOTHING_WEIGHT) * last_smoother_ascent_rate;
-    last_smoother_ascent_rate = smoothed_ascent_rate;
-    Serial.print("Smoothed ascent rate: ");
-    Serial.print(smoothed_ascent_rate);
-    Serial.println(" m/s");
-    */
 
     //beep(smoothed_ascent_rate);
+
+    /* Read input */
+    JOYSTICK b = readButtons(JOYSTICK_PIN);
+    if (b != NONE) Serial.println(b);
+
+    /* Changing state */
+    switch(vario_state){
+        case HOME: 
+            if(b == RIGHT) vario_state = HOME_VARIO;
+            else if(b == LEFT) vario_state = HOME_SETTINGS;
+            break;
+        case HOME_VARIO: 
+            if(b == RIGHT) vario_state = HOME_GPS;
+            else if(b == LEFT) vario_state = HOME;
+            else if(b == PRESS) vario_state = VARIO;
+            break;
+        case HOME_GPS: 
+            if(b == RIGHT) vario_state = HOME_COMPASS;
+            else if(b == LEFT) vario_state = HOME_VARIO;
+            else if(b == PRESS) vario_state = GPS;
+            break;
+        case HOME_COMPASS: 
+            if(b == RIGHT) vario_state = HOME_ALL;
+            else if(b == LEFT) vario_state = HOME_GPS;
+            else if(b == PRESS) vario_state = COMPASS;
+            break;
+        case HOME_ALL: 
+            if(b == RIGHT) vario_state = HOME_SETTINGS;
+            else if(b == LEFT) vario_state = HOME_COMPASS;
+            else if(b == PRESS) vario_state = ALL;
+            break;
+        case HOME_SETTINGS: 
+            if(b == RIGHT) vario_state = HOME;
+            else if(b == LEFT) vario_state = HOME_ALL;
+            else if(b == PRESS) vario_state = SETTINGS;
+            break;
+        case VARIO: 
+
+            break;
+        case GPS: 
+
+            break;
+        case COMPASS: 
+
+            break;
+        case ALL: 
+
+            break;
+        case SETTINGS: 
+
+            break;
+    }
+
+    // TODO: add time check maybe, to refresh data when not on menu
+    if (vario_state != previous_vario_state) {
+        first_time_state = true;
+        previous_vario_state = vario_state;
+        long time = micros();
+        clearDisplay();
+        Serial.print("Clearing takes: ");
+        Serial.print((micros()-time)/1000.0);
+        Serial.println(" ms");
+    }
+    else first_time_state = false;
+
+    if (micros() > display_time + DISPLAY_TIME_US) {
+        time_to_display = true;
+        display_time = micros();
+    }
+    else time_to_display = false;
+
+    switch(vario_state){
+        case HOME: 
+            //time = micros();
+           if (time_to_display) displayMenu(); //displayStr(90, 80, "HOME");
+            /*
+            Serial.print("Writing takes: ");
+            Serial.print((micros()-time)/1000.0);
+            Serial.println(" ms");
+            */
+            break;
+        case HOME_VARIO:
+            if (time_to_display) displayStr(90, 80, "VARIO");
+            break;
+        case HOME_COMPASS:
+            if (time_to_display) displayStr(70, 80, "COMPASS");
+            break;
+        case HOME_GPS:
+            if (time_to_display) displayStr(90, 80, "GPS");
+            break;
+        case HOME_ALL:
+            if (time_to_display) displayStr(90, 80, "ALL");
+            break;
+        case HOME_SETTINGS:
+            if (time_to_display) displayStr(70, 80, "SETTINGS");
+            break;
+        case VARIO: 
+            if (first_time_state) {
+                displayStatusBar();
+                displayVarioPage();
+                /*
+                displayStr(5, STATUS_BAR_OFFSET_Y+5, "Variometer mode");
+                
+                displayStr(5, STATUS_BAR_OFFSET_Y+100, "Altitude: ");
+                displayStr(185, STATUS_BAR_OFFSET_Y+100, "m");
+                displayStr(5, STATUS_BAR_OFFSET_Y+120, "Ascent: ");
+                displayStr(185, STATUS_BAR_OFFSET_Y+120, "m/s");
+                */
+                barometer_time = micros();
+            }
+            float altitude = getAltitude();
+            float ascent_rate = getAscentRate(last_time);
+            float pressure = getAbsolutePressure();
+            float temperature = getTemperature();
+            
+            Serial.print("Ascent rate: ");
+            Serial.print(String(ascent_rate, 1));
+            Serial.println(" m/s");
+
+            float smoothed_ascent_rate = SMOOTHING_WEIGHT * ascent_rate + (1-SMOOTHING_WEIGHT) * last_smoother_ascent_rate;
+            last_smoother_ascent_rate = smoothed_ascent_rate;
+            avg_ascent_rate += smoothed_ascent_rate;
+            avg_ascent_rate /= 2.0;
+
+            Serial.print("Smoothed ascent rate: ");
+            Serial.print(String(smoothed_ascent_rate, 1));
+            Serial.println(" m/s");
+
+            if (time_to_display){       
+                
+                if (micros() > barometer_time + BAROMETER_TIME_US) {      
+                    // Test fit with max values
+                    //displayVarioInfos(10080.0, 1085.0, -20.5, ascent_rate);
+                    displayVarioInfos(altitude, pressure, temperature, ascent_rate);
+                    barometer_time = micros();
+                } 
+            }
+            break;
+        /*
+        case GPS: {
+            break;
+        }
+        case COMPASS: 
+            {}
+            break;
+        case ALL: 
+            {}
+            break;
+        case SETTINGS: 
+            {}
+            break;
+        */
+    }
+    //}
+
     
-    lastTime = millis();
-    delay(10);
+    last_time = millis();
+    //while(micros() < loop_time + LOOP_TIME_US) {}// wait 
+    /*
+    Serial.print("Loop time: ");
+    Serial.print((micros() - loop_time)/1000.0);
+    Serial.println("ms");
+    */
 }
 
-int readButtons(int pin) {
+JOYSTICK readButtons(int pin) {
     int c = analogRead(JOYSTICK_PIN);
-    int b = NONE;
-    if(c > 870) b = UP; 
-    else if(c < 840 && c > 800) b = LEFT;
-    else if(c < 660 && c > 600) b = DOWN;
-    else if(c < 580 && c > 520) b = PRESS;
-    else if(c < 450 && c > 400) b = RIGHT;
+    //Serial.println(c);
+    JOYSTICK b = NONE;
+    if(c > 2800) b = UP; 
+    else if(c < 2700 && c > 2600) b = LEFT;
+    else if(c < 2200 && c > 2100) b = DOWN;
+    else if(c < 2000 && c > 1900) b = PRESS;
+    else if(c < 1850 && c > 1750) b = RIGHT;
 
     if (joystick_previous_state != b){
         joystick_previous_state = b;
@@ -111,15 +269,29 @@ void beep(float smoothed_ascent_rate) {
             // Calculate freq
             curr_freq = smoothed_ascent_rate * final_freq / max_climb;
 
-            // 1/bps to get the duration, * 1000 to switch to ms
-            tone(BUZZER_PIN, curr_freq, curr_waiting_time);
+            // 1/bps to get the duration, * 1000 to switch to ms$
+            //TODO: to replace with 
+            /*
+            void setup() {
+                ledcSetup(0,1E5,12);
+                ledcAttachPin(25,0);
+                }
+                void loop() {
+                ledcWriteTone(0,800);
+                delay(1000);
+                uint8_t octave = 1;
+                ledcWriteNote(0,NOTE_C,octave);  
+                delay(1000);
+                }
+            */
+            //tone(BUZZER_PIN, curr_freq, curr_waiting_time);
         } 
         else if (smoothed_ascent_rate < sink_threshold) {
             //tone(BUZZER_PIN, 1000, 200);
         }
         else{
             curr_waiting_time = 1000.0;
-            tone(BUZZER_PIN, 400, curr_waiting_time);
+            //tone(BUZZER_PIN, 400, curr_waiting_time);
         }
         time_started_tone = millis();
     }
